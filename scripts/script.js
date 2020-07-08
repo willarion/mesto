c8ef2f55a73deee2923d80cf1661ca5156896d4a
Marina Anisimova <anisimova.marina.n@gmail.com>
2020-07-08 19:09:27 +0300

    refactor: переделать формулы открытия pop-up

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,40 @@
-// открытие и закрытие попапа. начало
+// все константы и переменные
 const editProfileBtn = document.querySelector('.profile__edit-btn');
 const closePopUpBtn = document.querySelector('.pop-up__reset-btn');
 const popUp = document.querySelector('.pop-up');
 
-function togglePopUp() {
-  popUp.classList.toggle('pop-up_visible');
+let formElement = document.querySelector('.pop-up__container');
+let nameInput = document.querySelector('.pop-up__name');
+let bioInput = document.querySelector('.pop-up__bio');
+let profileName = document.querySelector('.profile__name');
+let profileBio = document.querySelector('.profile__bio');
+
+// открытие и закрытие попапа
+function openPopUp() {
+  popUp.classList.add('pop-up_visible');
+  nameInput.value = profileName.textContent;
+  bioInput.value = profileBio.textContent;
 }
 
-editProfileBtn.addEventListener('click', togglePopUp);
-closePopUpBtn.addEventListener('click', togglePopUp);
-//конец
+function closePopUp() {
+  popUp.classList.remove('pop-up_visible');
+}
+
+editProfileBtn.addEventListener('click', openPopUp);
+closePopUpBtn.addEventListener('click', closePopUp);
+
 
 //работа кнопки "сохранить"
-let formElement = document.querySelector('.pop-up__container');
-
 function formSubmitHandler (evt) {
   evt.preventDefault(); 
 
-  let nameInput = document.querySelector('.pop-up__name');
-  let bioInput = document.querySelector('.pop-up__bio');
+//  let name = nameInput.value;
+//  let bio = bioInput.value;
 
-  let name = nameInput.value;
-  let bio = bioInput.value;
+  profileName.textContent = nameInput.value;;
+  profileBio.textContent = bioInput.value;
 
-  let profileName = document.querySelector('.profile__name');
-  let profileBio = document.querySelector('.profile__bio');
-
-  profileName.textContent = name;
-  profileBio.textContent = bio;
-
-  popUp.classList.remove('pop-up_visible');
+  closePopUp();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
