ba3c75315b4962fab2f41936e4f83c02a02dd44e
Marina Anisimova <anisimova.marina.n@gmail.com>
2020-07-20 23:11:08 +0300

    feat: добавить начальные 6 карточек

--- scripts/script.js
+++ scripts/script.js
@@ -38,3 +38,47 @@ function formSubmitHandler (evt) {
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+
+
+//добавление 6-ти начальных карточек
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+const cardTemplate = document.querySelector('.card-template').content; //содержание template
+const cardList = document.querySelector('.elements__list'); //секция, куда мы добавляем карточки
+
+function createCard () {  
+    for (let i = 0; i < initialCards.length; i += 1) {
+    const cardElement = cardTemplate.cloneNode(true); //копируем содержание template
+
+    cardElement.querySelector('.element__text').textContent = initialCards[i].name;
+    cardElement.querySelector('.element__image').src = initialCards[i].link;
+
+    cardList.append(cardElement);
+  }
+} 
+createCard ();
+
