ab65983c70bb61a627609d9352a0d4759cc93e5a
Marina Anisimova <anisimova.marina.n@gmail.com>
2020-08-31 16:01:05 +0300

    feat: подготовить html&css к валидации

---
+++ scripts/validate.js
@@ -0,0 +1,11 @@
+/* 
+enableValidation({
+  formSelector: '.modal__container',
+  inputSelector: '.modal__input-line',
+  submitButtonSelector: '."modal__save-btn',
+  inactiveButtonClass: 'modal__save-btn_disabled',
+  inputErrorClass: 'modal__input-line_type_error',
+  errorClass: 'modal__error_visible'
+});
+*/
+
