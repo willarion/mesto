a6cbe9643babf2208814b0eb6085b7cf144e9f87
Marina Anisimova <anisimova.marina.n@gmail.com>
2020-08-31 17:13:55 +0300

    feat: валидировать форму

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,11 +1,80 @@
-/* 
+
+const showInputError = (formElement, inputElement, errorMessage, {inputErrorClass, errorClass}) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.add(inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(errorClass);
+};
+
+const hideInputError = (formElement, inputElement, {inputErrorClass, errorClass}) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.remove(inputErrorClass);
+  errorElement.classList.remove(errorClass);
+  errorElement.textContent = '';
+};
+
+
+const checkInputValidity = (formElement, inputElement, rest) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage, rest);
+  } else {
+    hideInputError(formElement, inputElement, rest);
+  }
+};
+
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+
+const toggleButtonState = (inputList, buttonElement, {inactiveButtonClass}) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add(inactiveButtonClass);
+    buttonElement.disabled = true;
+  } else {
+    buttonElement.classList.remove(inactiveButtonClass);
+    buttonElement.disabled = false;
+  }
+};
+
+
+const setEventListeners = (formElement, {inputSelector, submitButtonSelector, ...rest}) => {
+  const inputList = Array.from(formElement.querySelectorAll(inputSelector));
+  const buttonElement = formElement.querySelector(submitButtonSelector);
+
+  toggleButtonState(inputList, buttonElement, rest);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement, rest);
+      toggleButtonState(inputList, buttonElement, rest);
+    });
+  });
+};
+
+
+const enableValidation = ({formSelector, ...rest}) => {
+  const formList = Array.from(document.querySelectorAll(formSelector));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });
+
+    setEventListeners(formElement, rest);
+  });
+};
+
+
 enableValidation({
   formSelector: '.modal__container',
   inputSelector: '.modal__input-line',
-  submitButtonSelector: '."modal__save-btn',
+  submitButtonSelector: '.modal__save-btn',
   inactiveButtonClass: 'modal__save-btn_disabled',
   inputErrorClass: 'modal__input-line_type_error',
   errorClass: 'modal__error_visible'
 });
-*/
+
 
