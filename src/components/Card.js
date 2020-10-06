e7f2b1f0a1ccf9c2e31320104c1b39b30b4d1edc
Marina Anisimova <anisimova.marina.n@gmail.com>
2020-10-07 00:22:36 +0300

    refactor: объединить слушатели для Card в один метод

--- src/components/Card.js
+++ src/components/Card.js
@@ -25,30 +25,26 @@ export class Card {
       this._cardImage.alt = this._name;
     } else {this._cardImage.alt = 'место';}
 
-    this._likeCardSetEventListeners();
-    this._deleteCardSetEventListeners();
-    this._openBigImageSetEventListeners(this._cardImage);
+    this._setEventListeners(this._cardImage);
 
     return this._cardElement;
   }
 
-  _likeCardSetEventListeners() {
-    this._cardElement.querySelector('.element__like').addEventListener('click', (evt) => {this._likeCard (evt)});
-  }
   _likeCard (evt) {
     const eventTarget = evt.target; 
     eventTarget.classList.toggle('element__like_is-liked');
   }
 
-  _deleteCardSetEventListeners() {
-    this._cardElement.querySelector('.element__delete-btn').addEventListener('click', (evt) => {this._deleteCard (evt)});
-  }
   _deleteCard(evt) {
     evt.target.closest('.card-element').remove();
   }
 
-  _openBigImageSetEventListeners(image) {
-   image.addEventListener('click', () => {this._handleCardClick(this._name, this._link)});
+  _setEventListeners(image) {
+    this._cardElement.querySelector('.element__like').addEventListener('click', (evt) => {this._likeCard (evt)});
+
+    this._cardElement.querySelector('.element__delete-btn').addEventListener('click', (evt) => {this._deleteCard (evt)});
+    
+    image.addEventListener('click', () => {this._handleCardClick(this._name, this._link)});
   }
 }
 
