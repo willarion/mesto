24e47e623ee4f53e16646784078dc49e628e4486
Marina Anisimova <anisimova.marina.n@gmail.com>
2020-10-06 23:13:55 +0300

    bugfix: исправить порядок добавления начальных карточек в Section, оптимизировать код Section

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,19 +2,21 @@ export class Section {
   constructor({items, renderer}, containerSelector) {
     this._items = items;
     this.renderer = renderer;
-    this.containerSelector = containerSelector; 
+    this._cardList = document.querySelector(containerSelector);
   }
 
   renderInitialCards() {
     this._items.forEach((cardItem) => {
-      this.addItem(this.renderer(cardItem));
+      this.addItem(this.renderer(cardItem), this._items);
     });
   }
 
-  addItem(cardElement) {
-
-    const cardList = document.querySelector(this.containerSelector);
-    cardList.prepend(cardElement);
+  addItem(cardElement, array) {
+    if (array) {
+      this._cardList.append(cardElement); 
+    } else {
+      this._cardList.prepend(cardElement);
+    }    
   }
 }
 
